7c6b4eeb71e839fd2e102496d8e2943caa3b5290
saibt <saibt@tutanota.com>
2015-10-06 15:44:33 -0400

    Update ip_id.sh

--- ip_id.sh
+++ ip_id.sh
@@ -9,7 +9,7 @@ else
    host ${TARGET} > ${TARGET}.log 2> ${TARGET}.log 
    if [ $? -eq 1 ]; then
       echo "Host ${TARGET} not found... exiting"
-      exit 4         
+      exit 1         
     else
        IPS=$(grep -i address ${TARGET}.log | cut -d " " -f4,5 | grep -v address)
        echo "The IP(s) were identified below:"
